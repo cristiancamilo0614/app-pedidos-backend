b65ce5492e9e727f4e577707746132168f10f9e0
cristian0614 <cristian.camilo0614@gmail.com>
2021-11-06 14:49:39 -0500

    version 1

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,65 @@
+import {belongsTo, Entity, model, property} from '@loopback/repository';
+import {Persona} from './persona.model';
+
+@model({settings: {strict: false}})
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  estado: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_persona: string;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @property({
+    type: 'string',
+  })
+  productoId?: string;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
